c9032cf9b6782d279248020499d9a5019f0d15c0
Hikaru Ikuta <woodrush924@gmail.com>
2022-08-22 18:33:09 +0900

    Add universal lambda backend

--- target/ulamb.c
+++ target/ulamb.c
@@ -1,49 +1,47 @@
 #include <ir/ir.h>
 #include <target/util.h>
-// #include <target/ulambcore.h>
+#include <target/ulambcore.h>
 
 // #define DEBUG
 
 
-
 // (cons x y) = (lambda (f) (f x y))
-// = ``s``si`k[x]`k[y]
-static const char CONS_HEAD[] = "``s``si`k";
-static const char CONS_COMMA[] = "`k";
+// = 00010110[x][y]
+static const char CONS_HEAD[] = "00010110";
+static const char CONS_COMMA[] = "";
 
 // (cons4 x1 x2 x3 x4) = (lambda (f) (f x1 x2 x3 x4))
-// = ``s``s``s``si`k[x1]`k[x2]`k[x3]`k[x4]
-static const char CONS4_HEAD[] = "``s``s``s``si`k";
+// = 000101010110[x1][x2][x3][x4]
+static const char CONS4_HEAD[] = "000101010110";
 
-static const char T[] = "k";
-static const char NIL[] = "`ki";
-static const char LAZY_REG_A[] = "``s`kk``s`kk``s`kk``s`kkk";
-static const char LAZY_REG_B[] = "`k``s`kk``s`kk``s`kkk";
-static const char LAZY_REG_C[] = "`k`k``s`kk``s`kkk";
-static const char LAZY_REG_D[] = "`k`k`k``s`kkk";
-static const char LAZY_REG_SP[] = "`k`k`k`kk";
-static const char LAZY_REG_BP[] = "`k`k`k`k`ki";
-static const char INST_IO_INT[] = "``s`kk``s`kk``s`kk``s`kk``s`kk``s`kk``s`kkk";
-static const char INST_SUB[] = "`k``s`kk``s`kk``s`kk``s`kk``s`kk``s`kkk";
-static const char INST_CMP[] = "`k`k``s`kk``s`kk``s`kk``s`kk``s`kkk";
-static const char INST_LOAD[] = "`k`k`k``s`kk``s`kk``s`kk``s`kkk";
-static const char INST_JUMPCMP[] = "`k`k`k`k``s`kk``s`kk``s`kkk";
-static const char INST_JMP[] = "`k`k`k`k`k``s`kk``s`kkk";
-static const char INST_MOV[] = "`k`k`k`k`k`k``s`kkk";
-static const char INST_STORE[] = "`k`k`k`k`k`k`kk";
-static const char INST_ADD[] = "`k`k`k`k`k`k`k`ki";
-static const char CMP_EQ[] = "``s`kk``s`kk``s`kk``s`kkk";
-static const char CMP_NE[] = "`k``s`kk``s`kk``s`kkk";
-static const char CMP_LT[] = "`k`k``s`kk``s`kkk";
-static const char CMP_GT[] = "`k`k`k``s`kkk";
-static const char CMP_LE[] = "`k`k`k`kk";
-static const char CMP_GE[] = "`k`k`k`k`ki";
-static const char IO_INT_EXIT[] = "``s`kkk";
-static const char IO_INT_GETC[] = "`kk";
-static const char IO_INT_PUTC[] = "`k`ki";
+static const char T[] = "0000110";
+static const char NIL[] = "000010";
+static const char ULAMB_REG_A[] = "0000000000001111110";
+static const char ULAMB_REG_B[] = "000000000000111110";
+static const char ULAMB_REG_C[] = "00000000000011110";
+static const char ULAMB_REG_D[] = "0000000000001110";
+static const char ULAMB_REG_SP[] = "000000000000110";
+static const char ULAMB_REG_BP[] = "00000000000010";
+static const char INST_IO_INT[] = "0000000000000000001111111110";
+static const char INST_SUB[] = "000000000000000000111111110";
+static const char INST_CMP[] = "00000000000000000011111110";
+static const char INST_LOAD[] = "0000000000000000001111110";
+static const char INST_JUMPCMP[] = "000000000000000000111110";
+static const char INST_JMP[] = "00000000000000000011110";
+static const char INST_MOV[] = "0000000000000000001110";
+static const char INST_STORE[] = "000000000000000000110";
+static const char INST_ADD[] = "00000000000000000010";
+static const char CMP_EQ[] = "0000000000001111110";
+static const char CMP_NE[] = "000000000000111110";
+static const char CMP_LT[] = "00000000000011110";
+static const char CMP_GT[] = "0000000000001110";
+static const char CMP_LE[] = "000000000000110";
+static const char CMP_GE[] = "00000000000010";
+static const char IO_INT_EXIT[] = "0000001110";
+static const char IO_INT_GETC[] = "000000110";
+static const char IO_INT_PUTC[] = "00000010";
 
-static void lazy_debug(const char* fmt, ...) {
-  printf("\n");
+static void ulamb_debug(const char* fmt, ...) {
   if (fmt[0]) {
     #ifndef DEBUG
       return;
@@ -55,37 +53,37 @@ static void lazy_debug(const char* fmt, ...) {
   }
 }
 
-static const char* lazy_reg(Reg r) {
+static const char* ulamb_reg(Reg r) {
   switch (r) {
-  case A: return LAZY_REG_A;
-  case B: return LAZY_REG_B;
-  case C: return LAZY_REG_C;
-  case D: return LAZY_REG_D;
-  case BP: return LAZY_REG_BP;
-  case SP: return LAZY_REG_SP;
+  case A: return ULAMB_REG_A;
+  case B: return ULAMB_REG_B;
+  case C: return ULAMB_REG_C;
+  case D: return ULAMB_REG_D;
+  case BP: return ULAMB_REG_BP;
+  case SP: return ULAMB_REG_SP;
   default:
     error("unknown register: %d", r);
   }
 }
 
-static void lazy_emit_int(int n) {
-  lazy_debug("\n# int %d (%0d)\n", n, n);
+static void ulamb_emit_int(int n) {
+  ulamb_debug("\n# int %d (%0d)\n", n, n);
   for (int i = 0; i < 24; i++) {
 #ifndef __eir__
     n &= ((1 << 24) - 1);
 #endif
     fputs(CONS_HEAD, stdout);
-    lazy_debug("    ");
+    ulamb_debug("    ");
     fputs((n & 1) ? T : NIL, stdout);
-    lazy_debug("    ");
+    ulamb_debug("    ");
     fputs(CONS_COMMA, stdout);
-    lazy_debug("\n");
+    ulamb_debug("\n");
     n = n >> 1;
   }
   fputs(NIL, stdout);
 }
 
-static void emit_lazy_isimm(Value* v) {
+static void emit_ulamb_isimm(Value* v) {
   if (v->type == REG) {
     fputs(NIL, stdout);
   } else if (v->type == IMM) {
@@ -95,30 +93,30 @@ static void emit_lazy_isimm(Value* v) {
   }
 }
 
-static void emit_lazy_value_str(Value* v) {
+static void emit_ulamb_value_str(Value* v) {
   if (v->type == REG) {
-    fputs(lazy_reg(v->reg), stdout);
+    fputs(ulamb_reg(v->reg), stdout);
   } else if (v->type == IMM) {
-    lazy_emit_int(v->imm);
+    ulamb_emit_int(v->imm);
   } else {
     error("invalid value");
   }
 }
 
-static void lazy_emit_data(Data* data) {
-  lazy_debug("# Data\n");
+static void ulamb_emit_data(Data* data) {
+  ulamb_debug("# Data\n");
   for (Data* d = data; d; d = d->next) {
-    lazy_debug("\n# data\n");
+    ulamb_debug("\n# data\n");
     fputs(CONS_HEAD, stdout);
-    lazy_emit_int(d->v);
+    ulamb_emit_int(d->v);
     fputs(CONS_COMMA, stdout);
   }
-  lazy_debug("\n# Data end\n");
+  ulamb_debug("\n# Data end\n");
   fputs(NIL, stdout);
 }
 
-static void lazy_emit_func_prologue(int func_id) {
-  lazy_debug("\n# Func_prologue\n");
+static void ulamb_emit_func_prologue(int func_id) {
+  ulamb_debug("\n# Func_prologue\n");
   // fputs(CONS_HEAD, stdout);
 
   // Placeholder code that does nothing, to suppress compilation errors
@@ -127,85 +125,85 @@ static void lazy_emit_func_prologue(int func_id) {
   }
 }
 
-static void lazy_emit_pc_change(int pc) {
+static void ulamb_emit_pc_change(int pc) {
   if (pc) {
-    lazy_debug("\n# PC change\n");
+    ulamb_debug("\n# PC change\n");
     fputs(NIL, stdout);
     fputs(CONS_COMMA, stdout);
     fputs(CONS_HEAD, stdout);
   }
 }
 
-static void lazy_emit_func_epilogue(void) {
-  lazy_debug("\n# Func_epilogue\n");
+static void ulamb_emit_func_epilogue(void) {
+  ulamb_debug("\n# Func_epilogue\n");
   // fputs(NIL, stdout);
   // fputs(CONS_COMMA, stdout);
   // fputs(NIL, stdout);
 }
 
-static void lazy_emit_basic_inst(Inst* inst, const char* inst_tag) {
+static void ulamb_emit_basic_inst(Inst* inst, const char* inst_tag) {
     fputs(CONS4_HEAD, stdout);
     fputs(inst_tag, stdout);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_isimm(&inst->src);
+    emit_ulamb_isimm(&inst->src);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->src);
+    emit_ulamb_value_str(&inst->src);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->dst);
+    emit_ulamb_value_str(&inst->dst);
 }
 
-static void lazy_emit_jumpcmp_inst(Inst* inst, const char* cmp_tag) {
+static void ulamb_emit_jumpcmp_inst(Inst* inst, const char* cmp_tag) {
     fputs(CONS4_HEAD, stdout);
     fputs(INST_JUMPCMP, stdout);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_isimm(&inst->src);
+    emit_ulamb_isimm(&inst->src);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->src);
+    emit_ulamb_value_str(&inst->src);
     fputs(CONS_COMMA, stdout);
     fputs(CONS4_HEAD, stdout);
     fputs(cmp_tag, stdout);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->dst);
+    emit_ulamb_value_str(&inst->dst);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_isimm(&inst->jmp);
+    emit_ulamb_isimm(&inst->jmp);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->jmp);
+    emit_ulamb_value_str(&inst->jmp);
 }
 
-static void lazy_emit_cmp_inst(Inst* inst, const char* cmp_tag) {
+static void ulamb_emit_cmp_inst(Inst* inst, const char* cmp_tag) {
     fputs(CONS4_HEAD, stdout);
     fputs(INST_CMP, stdout);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_isimm(&inst->src);
+    emit_ulamb_isimm(&inst->src);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->src);
+    emit_ulamb_value_str(&inst->src);
     fputs(CONS_COMMA, stdout);
     fputs(CONS_HEAD, stdout);
     fputs(cmp_tag, stdout);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->dst);
+    emit_ulamb_value_str(&inst->dst);
 }
 
 
-static void lazy_emit_inst(Inst* inst) {
-  lazy_debug("\n# Inst\n");
+static void ulamb_emit_inst(Inst* inst) {
+  ulamb_debug("\n# Inst\n");
   fputs(CONS_HEAD, stdout);
-  lazy_debug("\n# Inst-body (%d)\n", inst->op);
+  ulamb_debug("\n# Inst-body (%d)\n", inst->op);
   switch (inst->op) {
   case MOV:
-    lazy_emit_basic_inst(inst, INST_MOV);
+    ulamb_emit_basic_inst(inst, INST_MOV);
     break;
   case ADD:
-    lazy_emit_basic_inst(inst, INST_ADD);
+    ulamb_emit_basic_inst(inst, INST_ADD);
     break;
   case SUB:
-    lazy_emit_basic_inst(inst, INST_SUB);
+    ulamb_emit_basic_inst(inst, INST_SUB);
     break;
   case LOAD:
-    lazy_emit_basic_inst(inst, INST_LOAD);
+    ulamb_emit_basic_inst(inst, INST_LOAD);
     break;
   case STORE:
-    lazy_emit_basic_inst(inst, INST_STORE);
+    ulamb_emit_basic_inst(inst, INST_STORE);
     break;
 
   case PUTC: //5
@@ -213,9 +211,9 @@ static void lazy_emit_inst(Inst* inst) {
     fputs(CONS4_HEAD, stdout);
     fputs(INST_IO_INT, stdout);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_isimm(&inst->src);
+    emit_ulamb_isimm(&inst->src);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->src);
+    emit_ulamb_value_str(&inst->src);
     fputs(CONS_COMMA, stdout);
     fputs(IO_INT_PUTC, stdout);
     break;
@@ -226,7 +224,7 @@ static void lazy_emit_inst(Inst* inst) {
     fputs(CONS_COMMA, stdout);
     fputs(NIL, stdout);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->dst);
+    emit_ulamb_value_str(&inst->dst);
     fputs(CONS_COMMA, stdout);
     fputs(IO_INT_GETC, stdout);
     break;
@@ -248,57 +246,57 @@ static void lazy_emit_inst(Inst* inst) {
     fputs(CONS_COMMA, stdout);
     fputs(NIL, stdout);
     fputs(CONS_COMMA, stdout);
-    fputs(LAZY_REG_A, stdout);
+    fputs(ULAMB_REG_A, stdout);
     fputs(CONS_COMMA, stdout);
-    fputs(LAZY_REG_A, stdout);
+    fputs(ULAMB_REG_A, stdout);
     break;
 
   // (cons4 inst-cmp [src-isimm] [src] (cons [emum-cmp] [dst]))
   case EQ:
-    lazy_emit_cmp_inst(inst, CMP_EQ);
+    ulamb_emit_cmp_inst(inst, CMP_EQ);
     break;
   case NE: //10
-    lazy_emit_cmp_inst(inst, CMP_NE);
+    ulamb_emit_cmp_inst(inst, CMP_NE);
     break;
   case LT:
-    lazy_emit_cmp_inst(inst, CMP_LT);
+    ulamb_emit_cmp_inst(inst, CMP_LT);
     break;
   case GT:
-    lazy_emit_cmp_inst(inst, CMP_GT);
+    ulamb_emit_cmp_inst(inst, CMP_GT);
     break;
   case LE:
-    lazy_emit_cmp_inst(inst, CMP_LE);
+    ulamb_emit_cmp_inst(inst, CMP_LE);
     break;
   case GE: //14
-    lazy_emit_cmp_inst(inst, CMP_GE);
+    ulamb_emit_cmp_inst(inst, CMP_GE);
     break;
 
   case JEQ:
-    lazy_emit_jumpcmp_inst(inst, CMP_EQ);
+    ulamb_emit_jumpcmp_inst(inst, CMP_EQ);
     break;
   case JNE:
-    lazy_emit_jumpcmp_inst(inst, CMP_NE);
+    ulamb_emit_jumpcmp_inst(inst, CMP_NE);
     break;
   case JLT:
-    lazy_emit_jumpcmp_inst(inst, CMP_LT);
+    ulamb_emit_jumpcmp_inst(inst, CMP_LT);
     break;
   case JGT:
-    lazy_emit_jumpcmp_inst(inst, CMP_GT);
+    ulamb_emit_jumpcmp_inst(inst, CMP_GT);
     break;
   case JLE:
-    lazy_emit_jumpcmp_inst(inst, CMP_LE);
+    ulamb_emit_jumpcmp_inst(inst, CMP_LE);
     break;
   case JGE:
-    lazy_emit_jumpcmp_inst(inst, CMP_GE);
+    ulamb_emit_jumpcmp_inst(inst, CMP_GE);
     break;
 
   case JMP:
     fputs(CONS4_HEAD, stdout);
     fputs(INST_JMP, stdout);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_isimm(&inst->jmp);
+    emit_ulamb_isimm(&inst->jmp);
     fputs(CONS_COMMA, stdout);
-    emit_lazy_value_str(&inst->jmp);
+    emit_ulamb_value_str(&inst->jmp);
     fputs(CONS_COMMA, stdout);
     fputs(NIL, stdout);
     break;
@@ -306,23 +304,23 @@ static void lazy_emit_inst(Inst* inst) {
   default:
     error("oops");
   }
-  lazy_debug("\n# Inst-end\n");
+  ulamb_debug("\n# Inst-end\n");
   fputs(CONS_COMMA, stdout);
 }
 
 void target_ulamb(Module* module) {
   #ifndef DEBUG
-    printf("``");
-    // fputs(lazy_core, stdout);
+    printf("0101");
+    fputs(ulamb_core, stdout);
   #endif
 
-  lazy_emit_data(module->data);
+  ulamb_emit_data(module->data);
   fputs(CONS_HEAD, stdout);
   emit_chunked_main_loop(module->text,
-                        lazy_emit_func_prologue,
-                        lazy_emit_func_epilogue,
-                        lazy_emit_pc_change,
-                        lazy_emit_inst);
+                        ulamb_emit_func_prologue,
+                        ulamb_emit_func_epilogue,
+                        ulamb_emit_pc_change,
+                        ulamb_emit_inst);
   fputs(NIL, stdout);
   fputs(CONS_COMMA, stdout);
   fputs(NIL, stdout);
