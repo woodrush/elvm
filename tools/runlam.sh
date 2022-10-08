43e0afde2dc09aae9d02206b07841144a398ffed
Hikaru Ikuta <woodrush924@gmail.com>
2022-10-08 17:28:43 +0900

    Rename to packbits.c

--- tools/runlam.sh
+++ tools/runlam.sh
@@ -16,17 +16,17 @@ if [ ! -e out/uni++ ]; then
     sed -i 's/1024/16777216/g' lam2bin.c
     gcc lam2bin.c -o lam2bin
 
-    cp $orig_dir/tools/asc2bin.c .
-    gcc asc2bin.c -o asc2bin
+    cp $orig_dir/tools/packbits.c .
+    gcc packbits.c -o packbits
 
     cd $orig_dir
     mv $dir/binary-lambda-calculus/uni out/uni++
     mv $dir/lam2bin out
-    mv $dir/asc2bin out
+    mv $dir/packbits out
     rm -rf $dir
 fi
 
-(cat $1 | out/lam2bin | out/asc2bin; cat) | out/uni++ -o
-# cat $1 | out/lam2bin #| out/asc2bin; cat) | out/unid -o
-# (./out/blc blc $1 | out/asc2bin; cat) | out/unid -o
+(cat $1 | out/lam2bin | out/packbits; cat) | out/uni++ -o
+# cat $1 | out/lam2bin #| out/packbits; cat) | out/unid -o
+# (./out/blc blc $1 | out/packbits; cat) | out/unid -o
 
