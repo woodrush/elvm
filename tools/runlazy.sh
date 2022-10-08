5a9f0f7346f257427907620ca7882002f7c61454
Hikaru Ikuta <woodrush924@gmail.com>
2022-10-08 16:31:43 +0900

    Clone the repo in runlazy.sh, add -u option

--- tools/runlazy.sh
+++ tools/runlazy.sh
@@ -2,17 +2,19 @@
 
 set -e
 
+
 if [ ! -e out/lazyk ]; then
     orig_dir=$(pwd)
     dir=$(mktemp -d)
     cd $dir
 
-    wget https://raw.githubusercontent.com/irori/lazyk/master/lazyk.c
-    gcc -O2 lazyk.c -o lazyk
+    git clone https://github.com/irori/lazyk
+    cd lazyk
+    make
 
     mv lazyk ${orig_dir}/out
     cd $orig_dir
     rm -fr $dir
 fi
 
-out/lazyk $1
+out/lazyk -u $1
