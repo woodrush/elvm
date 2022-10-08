6aeffcd26a33fdcca6fd68c6ab0afbbc199e8b36
woodrush <woodrush924@gmail.com>
2022-10-08 20:03:41 +0900

    Rename interpreter

--- tools/runblc.sh
+++ tools/runblc.sh
@@ -8,7 +8,7 @@ if [ ! -e out/packbits ]; then
     mv packbits out
 fi
 
-if [ ! -e out/uni++ ]; then
+if [ ! -e out/uni ]; then
     orig_dir=$(pwd)
     dir=$(mktemp -d)
     cd $dir
@@ -16,7 +16,7 @@ if [ ! -e out/uni++ ]; then
     git clone https://github.com/melvinzhang/binary-lambda-calculus
     cd binary-lambda-calculus
     make > /dev/null
-    mv uni ${orig_dir}/out/uni++
+    mv uni ${orig_dir}/out
 
     cd $orig_dir
     rm -rf $dir
@@ -25,4 +25,4 @@ fi
 # Required for parsing large programs
 ulimit -s 524288
 
-(cat $1 | ./out/packbits; cat - ) | out/uni++ -o
+(cat $1 | ./out/packbits; cat - ) | out/uni -o
