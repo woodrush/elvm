a034b8277b31f38ab56088613cb41060f78e826c
woodrush <woodrush924@gmail.com>
2022-10-08 20:29:09 +0900

    Rename interpreter

--- tools/runlam.sh
+++ tools/runlam.sh
@@ -22,7 +22,7 @@ if [ ! -e out/lam2bin ]; then
     rm -rf $dir
 fi
 
-if [ ! -e out/uni++ ]; then
+if [ ! -e out/uni ]; then
     orig_dir=$(pwd)
     dir=$(mktemp -d)
     cd $dir
@@ -31,11 +31,11 @@ if [ ! -e out/uni++ ]; then
     make > /dev/null
 
     cd $orig_dir
-    mv $dir/binary-lambda-calculus/uni out/uni++
+    mv $dir/binary-lambda-calculus/uni out/uni
     rm -rf $dir
 fi
 
 # Required for parsing large programs
 ulimit -s 524288
 
-(cat $1 | out/lam2bin | out/packbits; cat) | out/uni++ -o
+(cat $1 | out/lam2bin | out/packbits; cat) | out/uni -o
