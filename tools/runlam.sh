a8402f4a46b122b6e0dfd5968631770b8fc468f5
Hikaru Ikuta <woodrush924@gmail.com>
2022-10-08 17:31:54 +0900

    Refine runlam.sh

--- tools/runlam.sh
+++ tools/runlam.sh
@@ -3,30 +3,36 @@
 set -e
 
 
+if [ ! -e out/packbits ]; then
+    gcc tools/packbits.c -o packbits > /dev/null
+    mv packbits out
+fi
+
+if [ ! -e out/lam2bin ]; then
+    orig_dir=$(pwd)
+    dir=$(mktemp -d)
+    cd $dir
+
+    wget https://justine.lol/lambda/lam2bin.c
+    sed -i 's/1024/16777216/g' lam2bin.c
+    gcc lam2bin.c -o lam2bin > /dev/null
+
+    cd $orig_dir
+    mv $dir/lam2bin out
+    rm -rf $dir
+fi
+
 if [ ! -e out/uni++ ]; then
     orig_dir=$(pwd)
     dir=$(mktemp -d)
     cd $dir
     git clone https://github.com/melvinzhang/binary-lambda-calculus
     cd binary-lambda-calculus
-    make
-
-    cd ..
-    wget https://justine.lol/lambda/lam2bin.c
-    sed -i 's/1024/16777216/g' lam2bin.c
-    gcc lam2bin.c -o lam2bin
-
-    cp $orig_dir/tools/packbits.c .
-    gcc packbits.c -o packbits
+    make > /dev/null
 
     cd $orig_dir
     mv $dir/binary-lambda-calculus/uni out/uni++
-    mv $dir/lam2bin out
-    mv $dir/packbits out
     rm -rf $dir
 fi
 
 (cat $1 | out/lam2bin | out/packbits; cat) | out/uni++ -o
-# cat $1 | out/lam2bin #| out/packbits; cat) | out/unid -o
-# (./out/blc blc $1 | out/packbits; cat) | out/unid -o
-
